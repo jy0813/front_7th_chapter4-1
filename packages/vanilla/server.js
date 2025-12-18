5cc4f39b8e28f030b9fa1a7335ba00529e82b5a2
jy0813 <busbug3@gmail.com>
2025-12-18 22:34:33 +0900

    feat: Express SSR 서버 적용

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,34 +1,60 @@
+import fs from "node:fs/promises";
 import express from "express";
 
-const prod = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
-const base = process.env.BASE || (prod ? "/front_7th_chapter4-1/vanilla/" : "/");
+const base = process.env.BASE || (isProduction ? "/front_7th_chapter4-1/vanilla/" : "/");
+
+const templateHtml = isProduction ? await fs.readFile("./dist/vanilla/index.html", "utf-8") : "";
 
 const app = express();
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+/** @type {import('vite').ViteDevServer | undefined} */
+let vite;
+if (!isProduction) {
+  const { createServer } = await import("vite");
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+    base,
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import("compression")).default;
+  const sirv = (await import("sirv")).default;
+  app.use(compression());
+  app.use(base, sirv("./dist/vanilla", { extensions: [] }));
+}
 
-app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+app.use("*all", async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, "/");
+
+    let template, render;
+    if (!isProduction) {
+      template = await fs.readFile("./index.html", "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule("/src/main-server.js")).render;
+    } else {
+      template = templateHtml;
+      render = (await import("./dist/vanilla-ssr/main-server.js")).render;
+    }
+
+    const rendered = await render(url, req.query);
+
+    const html = template
+      .replace("<!--app-head-->", rendered.head ?? "")
+      .replace("<!--app-html-->", rendered.html ?? "")
+      .replace("</head>", `${rendered.script ?? ""}</head>`);
+
+    res.status(200).set({ "Content-Type": "text/html" }).send(html);
+  } catch (e) {
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
+  }
 });
 
-// Start http server
 app.listen(port, () => {
-  console.log(`React Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
