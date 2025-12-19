84f64d063b76a3ad6b9a7fcd08f508e593d81d4c
jy0813 <busbug3@gmail.com>
2025-12-19 16:28:33 +0900

    fix: useSyncExternalStore에 getServerSnapshot 추가
      - useRouter, useStore에 세 번째 인자(getServerSnapshot) 추가
      - SSR hydration 시 'Missing getServerSnapshot' 에러 해결
      - 서버/클라이언트 간 상태 일관성 보장

--- packages/lib/src/hooks/useRouter.ts
+++ packages/lib/src/hooks/useRouter.ts
@@ -7,5 +7,7 @@ const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
 export const useRouter = <T extends RouterInstance<AnyFunction>, S>(router: T, selector = defaultSelector<T, S>) => {
   const shallowSelector = useShallowSelector(selector);
-  return useSyncExternalStore(router.subscribe, () => shallowSelector(router));
+  const getSnapshot = () => shallowSelector(router);
+  // SSR hydration을 위해 getServerSnapshot 제공 (getSnapshot과 동일)
+  return useSyncExternalStore(router.subscribe, getSnapshot, getSnapshot);
 };
